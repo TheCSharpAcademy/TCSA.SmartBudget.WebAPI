359e080c6759eb2a996f63dc6389fb80b5e489d2
thecsharpacademy <thecsharpacademy@gmail.com>
2025-12-28 23:11:38 +1000

    Auth 0 wired

---
+++ TCSA.SmartBudget.WebAPI/Options/AuthSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace TCSA.SmartBudget.WebAPI.Options;
+
+public sealed class Auth0Settings
+{
+    public string Domain { get; set; } = string.Empty;
+    public string Audience { get; set; } = string.Empty;
+}
+
--- TCSA.SmartBudget.WebAPI/Program.cs
+++ TCSA.SmartBudget.WebAPI/Program.cs
@@ -1,15 +1,34 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
 using TCSA.SmartBudget.WebAPI.Models;
+using TCSA.SmartBudget.WebAPI.Options;
 using TCSA.SmartBudget.WebAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.Configure<Auth0Settings>(builder.Configuration.GetSection("Auth0"));
+builder.Services.Configure<ConnectionStringsOptions>(builder.Configuration.GetSection("ConnectionStrings"));
+
 builder.Services.AddControllers();
 
-builder.Services.AddDbContext<BudgetContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+var auth0 = builder.Configuration.GetSection("Auth0").Get<Auth0Settings>()!;
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = $"https://{auth0.Domain}/";
+        options.Audience = auth0.Audience;
+    });
+
+builder.Services.AddDbContext<BudgetContext>((sp, db) =>
+{
+    var cs = sp.GetRequiredService<IOptions<ConnectionStringsOptions>>().Value.DefaultConnection;
+    db.UseSqlServer(cs);
+});
 
 builder.Services.AddScoped<IBudgetService, BudgetService>();
 
@@ -27,6 +46,7 @@ using (var scope = app.Services.CreateScope())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
